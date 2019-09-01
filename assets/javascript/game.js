1f2178b77d48c6755c6d09c037e28f3ec3dd01e5
Blake Sykes <blakesykes13@gmail.com>
2019-08-31 19:23:23 -0600

    added local bootstrap so i can work remotely... trying to link Javascript. Something isn't working...

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+
+console.log("JS READY")
+
+//var words = ["square","circle","triangle","rectangle","octagon","hexagon","pentagon","polygon"]
+
+//console.log(words)
+
+//var guesses = 12
+
+
+
+
